6dc5432348bd499eae982f75cbc2d2b31fae1956
jchan3 <joechan2014@gmail.com>
2016-10-29 05:42:26 +0000

    TASK 02

--- task_01.py
+++ task_01.py
@@ -3,18 +3,23 @@
 """Docstring for task_01."""
 
 
-GRADE_DATA = {'Luke Skywalker':
-              {'math': 'B',
-               'etiquette': 'B+',
-               'grammar': 'B',
-               'gym': 'A'},
-              'Han Solo':
-              {'math': 'A-',
-               'etiquette': 'C-',
-               'grammar': 'B',
-               'gym': 'B'},
-              'C-3PO':
-              {'math': 'C',
-               'etiquette': 'A+',
-               'grammar': 'A',
-               'gym': 'F'}}
+GRADE_DATA = {
+    'Luke Skywalker': {
+        'math': 'B',
+        'etiquette': 'B+',
+        'grammar': 'B',
+        'gym': 'A'
+    },
+    'Han Solo': {
+        'math': 'A-',
+        'etiquette': 'C-',
+        'grammar': 'B',
+        'gym': 'B'
+    },
+    'C-3PO': {
+        'math': 'C',
+        'etiquette': 'A+',
+        'grammar': 'A',
+        'gym': 'F'
+    }
+}
---
+++ task_02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for task_02."""
+
+
+import data
+
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+BAND_NAMES = (data.BANDS).keys()
